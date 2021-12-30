99c2eb23e679676e4c1266a213902b10b807ba04
Ali Hassan <ahassan@inthekloud.com>
2021-12-30 23:32:42 +0200

    create counter component

---
+++ src/components/counter.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  state = { count: 0 };
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getClassName()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </React.Fragment>
+    );
+  }
+  getClassName() {
+    var classes = "badge bg-";
+    return (classes += this.state.count === 0 ? "warning" : "primary");
+  }
+
+  formatCount() {
+    return this.state.count === 0 ? (
+      <h6>Zero</h6>
+    ) : (
+      <h6> {this.state.count}</h6>
+    );
+  }
+}
+
+export default Counter;
