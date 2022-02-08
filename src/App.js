aef0cbbf37e882c945eef9fb8f189fb559a727c2
Ali Hassan <ahassan@inthekloud.com>
2022-02-08 16:35:13 +0200

    use react context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { Component } from "react";
+import React, { Component, createContext } from "react";
 import NavBar from "./components/navbar";
 import Counters from "./components/counters";
 import { render } from "@testing-library/react";
@@ -11,27 +11,30 @@ import MovieDetailsPage from "./components/moviesDetailsPage/moviesDetailsPage";
 import LoginForm from "./components/loginPage/loginForm";
 import RegistrationForm from "./components/registrationPage/registrationForm";
 
+export const Theme = createContext("light");
 class App extends Component {
   render() {
     return (
-      <div>
-        <NavBar />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginForm />} />
-            <Route path="/Registration" element={<RegistrationForm />} />
-            <Route path="/movies" element={<MoviesPage />} />
-            <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-              {/* there is no optional params in react router dom v6 ,
+      <Theme.Provider value="dark">
+        <div>
+          <NavBar />
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/login" element={<LoginForm />} />
+              <Route path="/Registration" element={<RegistrationForm />} />
+              <Route path="/movies" element={<MoviesPage />} />
+              <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+                {/* there is no optional params in react router dom v6 ,
                 use nested routed with new param ,or new route with same name including the param  */}
-              {/* <Route path=":showNextMovie" element={<MovieDetailsPage />} /> */}
-            </Route>
-            {/* redirect to Not found */}
-            <Route path="*" element={<div>Not found</div>} />
-          </Routes>
+                {/* <Route path=":showNextMovie" element={<MovieDetailsPage />} /> */}
+              </Route>
+              {/* redirect to Not found */}
+              <Route path="*" element={<div>Not found</div>} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      </Theme.Provider>
     );
   }
 }
--- src/components/moviesDetailsPage/moviesDetailsPage.jsx
+++ src/components/moviesDetailsPage/moviesDetailsPage.jsx
@@ -1,18 +1,38 @@
 import { useParams, useSearchParams } from "react-router-dom";
-import React from "react";
+import React, { useState, useMemo, useContext } from "react";
+import { Theme } from "../../App";
 const MoviesDetailsPage = () => {
   // get params
   let { movieId, showNextMovie } = useParams();
+  const [counter, setCounter] = useState(0);
+  const theme = useContext(Theme);
 
   //get query params
   const [searchParams] = useSearchParams();
   console.log(searchParams.get("sort"));
+
+  //use memo button
+  const button = useMemo(() => {
+    return (
+      <>
+        <button
+          className="btn btn-success"
+          onClick={() => setCounter(counter + 1)}
+        >
+          Inc
+        </button>
+        Theme {theme}
+      </>
+    );
+  }, [counter]);
 
   return (
     <div>
       <h1>{movieId}</h1>
       <h1>{showNextMovie}</h1>
       <h1>QueryParam : {searchParams.get("sort")}</h1>
+      <h1>Counter : {counter}</h1>
+      {button}
     </div>
   );
 };
